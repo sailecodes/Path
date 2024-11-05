21d5be011bba8b8cda6cc1f114ab3c096e0bef29
saile <eliasiv.codes@gmail.com>
2024-11-05 00:51:17 -0800

    create general layout of home page

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,28 +1,40 @@
-import CustomInput from "@/components/custom-input";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  FlatListComponent,
+  ScrollView,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomInput from "@/components/custom-input";
 
 const Home = () => {
   return (
-    <SafeAreaView className="bg-green-200 flex-1">
+    <SafeAreaView className="bg-neutral-100 flex-1 gap-5">
       <CustomInput
-        className="flex mb-5"
+        className="flex"
         placeholder="Placeholder"
       />
-      <View className="flex-row px-5 mb-5 gap-5">
-        <TouchableOpacity className="bg-red-300 w-[70] h-[30] p-5 rounded-lg">
-          <Text className="text-neutral-800 font-JakartaMedium">Hello</Text>
+      <ScrollView
+        className="px-5"
+        horizontal
+        contentContainerStyle={{ flexDirection: "row", gap: 10 }}
+        showsHorizontalScrollIndicator={false}>
+        <TouchableOpacity className="bg-neutral-100 px-5 py-2 border-neutral-200 border rounded-lg">
+          <Text className="text-neutral-800 font-JakartaBold text-lg">Recent visits</Text>
         </TouchableOpacity>
-        <TouchableOpacity className="bg-red-300 w-[70] h-[30] p-5 rounded-lg">
-          <Text>Hello</Text>
+        <TouchableOpacity className="bg-neutral-100 px-5 py-2 border-neutral-200 border rounded-lg">
+          <Text className="text-neutral-800 font-JakartaBold text-lg">Current orders</Text>
         </TouchableOpacity>
-        <TouchableOpacity className="bg-red-300 w-[70] h-[30] p-5 rounded-lg">
-          <Text>Hello</Text>
+        <TouchableOpacity className="bg-neutral-100 px-5 py-2 border-neutral-200 border rounded-lg">
+          <Text className="text-neutral-800 font-JakartaBold text-lg">Vaccinations</Text>
         </TouchableOpacity>
-      </View>
-      <ScrollView>
-        <View className="bg-red-300 gap-5">
-          <View className="bg-yellow-300 mx-5 p-5 rounded-lg">
+      </ScrollView>
+      <ScrollView className="px-5">
+        <View className="gap-5 mb-5">
+          <View className="p-5 border-neutral-200 border rounded-lg">
             <Text className="text-neutral-800 font-JakartaSemiBold text-2xl mb-2">
               View medical history
             </Text>
@@ -31,16 +43,16 @@ const Home = () => {
               vaccination records, and more
             </Text>
           </View>
-          <View className="bg-yellow-300 mx-5 p-5 rounded-lg">
+          <View className="p-5 border-neutral-200 border rounded-lg">
             <Text className="text-neutral-800 font-JakartaSemiBold text-2xl mb-2">
               Make an appointment
             </Text>
             <Text className="text-neutral-800 font-JakartaMedium text-lg">
-              Set up an appointment with your doctor to ensure your health and medical record is
+              Book an appointment with your doctor to ensure your health and medical record is
               up-to-date
             </Text>
           </View>
-          <View className="bg-yellow-300 mx-5 p-5 rounded-lg">
+          <View className="p-5 border-neutral-200 border rounded-lg">
             <Text className="text-neutral-800 font-JakartaSemiBold text-2xl mb-2">
               Order from a pharmacy
             </Text>
@@ -48,6 +60,23 @@ const Home = () => {
               Purchase medications and health essentials from a preferred pharmacy for pickup or
               delivery
             </Text>
+          </View>
+        </View>
+        <View className="">
+          <Text className="text-neutral-800 font-JakartaBold text-3xl">Health recommendations</Text>
+          <View className="flex-row flex-wrap">
+            <TouchableOpacity className="justify-center items-center w-1/2 h-[100] border-neutral-200 border rounded-lg">
+              <Text className="font-JakartaSemiBold text-lg">Activities</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="justify-center items-center w-1/2 h-[100] border-neutral-200 border rounded-lg">
+              <Text className="font-JakartaSemiBold text-lg">Vaccines</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="justify-center items-center w-1/2 h-[100] border-neutral-200 border rounded-lg">
+              <Text className="font-JakartaSemiBold text-lg">Nutrition</Text>
+            </TouchableOpacity>
+            <TouchableOpacity className="justify-center items-center w-1/2 h-[100] border-neutral-200 border rounded-lg">
+              <Text className="font-JakartaSemiBold text-lg">Something</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
